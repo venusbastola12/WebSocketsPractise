b880a8f679f4f993a0902e98f9fe096ed9a728b4
Venus <venusbastola27@gmail.com>
2023-11-12 14:39:23 +0545

    comments added

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server); // here we create a instance of a socket server.
 io.on("connection", (socket) => {
+  // when the socket server is connected to any client or sockets then we apply following
   socket.on("user-message", (message) => {
     console.log("new message from user:", message);
-    io.emit("message", message);
+    io.emit("message", message); //it is used to emit message to all the client connected to the host
   });
   // socket.on("disconnect", () => {
   //   console.log("user disconnected from this tab");
